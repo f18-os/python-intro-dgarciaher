54fac4983e891fb2dd92bc04dc9af2566ea2f900
Dani <danielagarciaher98@gmail.com>
2018-09-19 12:38:51 -0600

    added change directories, background runs, and infinite loop

--- Shell/shell.py
+++ Shell/shell.py
@@ -2,63 +2,76 @@
 
 import os, sys, time, re
 
-userInput = input('$')
+while True:
 
-pid = os.fork()
+	wd = os.getcwd().split("/")
+	
+	userInput = input(wd[(len(wd)-1)] + '$')
+	if 'exit' in userInput :
+		break
 
-if pid < 0:
-	os.write(2, ('Fork failed').encode())
-	sys.exit(1)
+	if 'cd' in userInput :
+		change = userInput.split()
+		os.chdir(change[1])
+		continue
 
-elif pid == 0:
-	if '>' in userInput:
-		intputArray= userInput.split('>')
-		os.close(1)
-		sys.stdout = open(intputArray[1].strip(), "w")
-		fd = sys.stdout.fileno()
-		os.set_inheritable(fd,True)
 
-		parent = intputArray[0].split()
+	pid = os.fork()
 
-		for dir in re.split(":", os.environ['PATH']):
-			program = "%s/%s" % (dir, parent[0])
-			try:
-				os.execve(program, parent, os.environ)
-			except FileNotFoundError:
-				pass
-
-		os.write(2, ("Child:    Error: Could not exec %s\n" % parent[0]).encode())
+	if pid < 0:
+		os.write(2, ('Fork failed').encode())
 		sys.exit(1)
-	elif '<' in userInput:
-		intputArray= userInput.split('<')
-		os.close(0)
-		sys.stdin = open(intputArray[1].strip(), "r")
-		fd = sys.stdin.fileno()
-		os.set_inheritable(fd,True)
 
-		parent = intputArray[0].split()
-		print (parent)
-		for dir in re.split(":", os.environ['PATH']):
+	elif pid == 0:
+		if '>' in userInput:
+			intputArray= userInput.split('>')
+			os.close(1)
+			sys.stdout = open(intputArray[1].strip(), "w")
+			fd = sys.stdout.fileno()
+			os.set_inheritable(fd,True)
+
+			parent = intputArray[0].split()
+
+			for dir in re.split(":", os.environ['PATH']):
 				program = "%s/%s" % (dir, parent[0])
 				try:
 					os.execve(program, parent, os.environ)
 				except FileNotFoundError:
 					pass
 
-		os.write(2, ("Child:    Error: Could not exec %s\n" % parent[0]).encode())
-		sys.exit(1)
+			os.write(2, ("Child:    Error: Could not exec %s\n" % parent[0]).encode())
+			sys.exit(1)
+		elif '<' in userInput:
+			intputArray= userInput.split('<')
+			os.close(0)
+			sys.stdin = open(intputArray[1].strip(), "r")
+			fd = sys.stdin.fileno()
+			os.set_inheritable(fd,True)
+
+			parent = intputArray[0].split()
+			print (parent)
+			for dir in re.split(":", os.environ['PATH']):
+					program = "%s/%s" % (dir, parent[0])
+					try:
+						os.execve(program, parent, os.environ)
+					except FileNotFoundError:
+						pass
+
+			os.write(2, ("Child:    Error: Could not exec %s\n" % parent[0]).encode())
+			sys.exit(1)
+
+		else:
+			parent = userInput.replace("&","").split()
+			for dir in re.split(":", os.environ['PATH']):
+					program = "%s/%s" % (dir, parent[0])
+					try:
+						os.execve(program, parent, os.environ)
+					except FileNotFoundError:
+						pass
+
+			os.write(2, ("Child:    Error: Could not exec %s\n" % parent[0]).encode())
+			sys.exit(1)
 
 	else:
-		parent = userInput.split()
-		for dir in re.split(":", os.environ['PATH']):
-				program = "%s/%s" % (dir, parent[0])
-				try:
-					os.execve(program, parent, os.environ)
-				except FileNotFoundError:
-					pass
-
-		os.write(2, ("Child:    Error: Could not exec %s\n" % parent[0]).encode())
-		sys.exit(1)
-
-else:
-	pid: os.wait()  
+		if not '&' in userInput:
+		 	os.wait()
