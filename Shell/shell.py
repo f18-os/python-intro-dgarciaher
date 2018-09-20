a1a549b147054abca7c2e0e14f5c648fe231a0f7
Dani <danielagarciaher98@gmail.com>
2018-09-19 22:17:53 -0600

    everything working exept piping

--- Shell/shell.py
+++ Shell/shell.py
@@ -12,22 +12,20 @@ while True:
 		os.write(1, (wd[(len(wd)-1)] + '$').encode())		# writes path if previous method does not work
 
 	try :
-		userInput = input().split()		# checks for EOF when C^D is pressed by user
+		userInput = input()		# checks for EOF when C^D is pressed by user
 	except EOFError:
 		sys.exit(1)
 
-
-	if userInput == [] :		# continues the loop if user presses enter
+	if userInput == "":		# continues the loop if user presses enter
 		continue
 
 	if 'exit' in userInput :	# terminates if user enters exit
 		break
 
 	if 'cd' in userInput :		# changes directories
-		change = userInput
+		change = userInput.split()
 		os.chdir(change[1])
 		continue
-
 
 	pid = os.fork()
 
@@ -36,6 +34,10 @@ while True:
 		sys.exit(1)
 
 	elif pid == 0:		# is child
+		if "&" in userInput:		# checks if there's an & for the sleep command
+			#sleep logic
+			userInput = userInput.split("&")[0]
+			parent = userInput.split()
 		if '>' in userInput:
 			intputArray= userInput.split('>')				# checks for > symbol and executes command
 			os.close(1)
@@ -73,37 +75,24 @@ while True:
 			sys.exit(1)
 
 		else:
-			if userInput[-1] == "&":		# checks if there's an & for the sleep command
-				#sleep logic
-				userInput = userInput[0:-1]
-				parent = userInput
-				if "/" in parent[0]:			# checks for commands such as cat in a path
-					program = parent[0]
+			parent = userInput.split()
+			if "/" in parent[0]:			# checks for commands such as cat in a path
+				program = parent[0]
+				try:
+					os.execve(program, parent, os.environ)
+				except FileNotFoundError:
+					pass
+			else :
+				for dir in re.split(":", os.environ['PATH']):
+					program = "%s/%s" % (dir, parent[0])
 					try:
 						os.execve(program, parent, os.environ)
 					except FileNotFoundError:
 						pass
-				else :
-					for dir in re.split(":", os.environ['PATH']):
-						program = "%s/%s" % (dir, parent[0])
-						try:
-							os.execve(program, parent, os.environ)
-						except FileNotFoundError:
-							pass
 
-				os.write(2, ("Child:    Error: Could not exec %s\n" % parent[0]).encode())
-				sys.exit(1)
-			else:
-				parent = userInput
-				for dir in re.split(":", os.environ['PATH']):		# executes commands that do not fit in any of the above conditions
-						program = "%s/%s" % (dir, parent[0])
-						try:
-							os.execve(program, parent, os.environ)
-						except FileNotFoundError:
-							pass
-
-				os.write(2, ("Child:    Error: Could not exec %s\n" % parent[0]).encode())
-				sys.exit(1)				
+			os.write(2, ("Child:    Error: Could not exec %s\n" % parent[0]).encode())
+			sys.exit(1)
+			
 	else:
 		if not '&' in userInput:	# waits in the case of sleep command
 		 	os.wait()
